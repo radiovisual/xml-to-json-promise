9b609c8b052ed44113ed2daf5fd43b5dd8459025
Michael Wuergler <wuergler@gmail.com>
2016-04-09 17:03:03 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+var objectAssign = require('object-assign');
+var parseString = require('xml2js').parseString;
+
+require('native-promise-only');
+
+function xmlFileToJSON(filePath, xml2JsOptions) {
+	xml2JsOptions = objectAssign({}, xml2JsOptions);
+	return new Promise((resolve, reject) => {
+		fs.readFile(path.resolve(filePath), 'utf8', function (err, data) {
+			if (err) {
+				reject(err);
+			} else {
+				parseString(data, xml2JsOptions, function (err, result) {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(result);
+					}
+				});
+			}
+		});
+	});
+}
+
+function xmlDataToJSON(xmlData, xml2JsOptions) {
+	xml2JsOptions = objectAssign({}, xml2JsOptions);
+
+	return new Promise(function (resolve, reject) {
+		parseString(xmlData, xml2JsOptions, function (err, result) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(result);
+			}
+		});
+	});
+}
+
+module.exports = {
+	xmlFileToJSON,
+	xmlDataToJSON
+};
---
+++ test.js
@@ -0,0 +1,64 @@
+import test from 'ava';
+import convert from './index.js';
+
+const xmlSample = '<note><to>Manager</to><from>Developer</from><heading>Reminder</heading><body>Remember to write unit tests!</body></note>';
+const charSample = '<sample><chartest desc="Test for CHARs">Character data here!</chartest></sample>';
+
+test('should convert xml file to json', t => {
+	return convert.xmlFileToJSON('./fixtures/sample.xml').then(json => {
+		t.is(typeof json, 'object');
+		t.is(json.sample.chartest[0]._, 'Character data here!');
+	});
+});
+
+test('should convert data file to json', t => {
+	return convert.xmlDataToJSON(xmlSample).then(json => {
+		t.is(typeof json, 'object');
+		t.is(json.note.body[0], 'Remember to write unit tests!');
+	});
+});
+
+test('xmlFileToJSON should allow xml2js options', t => {
+	return convert.xmlFileToJSON('./fixtures/sample.xml', {charkey: '#'}).then(json => {
+		t.is(typeof json, 'object');
+		t.is(json.sample.chartest[0]['#'], 'Character data here!');
+	});
+});
+
+test('xmlDataToJSON should allow xml2js options', t => {
+	return convert.xmlDataToJSON(charSample, {charkey: '#'}).then(json => {
+		t.is(typeof json, 'object');
+		t.is(json.sample.chartest[0]['#'], 'Character data here!');
+	});
+});
+
+test('xmlFileToJSON rejects on error', async t => {
+	t.throws(convert.xmlFileToJSON('does-not-exist.xml'), /no such file or directory/igm);
+});
+
+test('xmlFileToJSON rejects on parse error', async t => {
+	t.throws(convert.xmlFileToJSON('./fixtures/invalid.xml'), /Unencoded </igm);
+});
+
+test('xmlDataToJSON rejects on error', async t => {
+	t.throws(convert.xmlDataToJSON('no json to parse'), /Non-whitespace before first tag/igm);
+});
+
+//  test('should convert raw xml data to json', t => {
+//	convert.xmlDataToJSON(xmlSample, false).then(json => {
+//		t.is(typeof json, 'object');
+//		t.is(json.body, 'Remember to write unit tests!');
+//	});
+//  });
+//
+//  test('xmlFileToJSON should allow xml2js options', t => {
+//	convert.xmlFileToJSON('./fixtures/sample.xml', false, ).then(data => {
+//		t.is(data, {});
+//	});
+//  });
+//
+//  test('xmlDataToJSON should allow xml2js options', t => {
+//	convert.xmlDataToJSON(xmlSample, false, {charkey: '#'}).then(data => {
+//		t.is(data, {});
+//	});
+//  });
